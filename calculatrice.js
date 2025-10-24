1cd469cf9a58ef9ff8be8a672db1a0d835a1940f
Milann lede <123367004+Milann-lede@users.noreply.github.com>
2025-10-24 11:20:13 +0200

    etape 1

---
+++ calculatrice.js
@@ -0,0 +1,19 @@
+const display = document.querySelector(".display");
+let currentValue = '';
+
+
+function NumberClick(event) {
+    const button = event.target;
+    currentValue += button.textContent;
+    display.textContent = currentValue;
+}
+
+
+document.querySelectorAll("[data-number]").forEach(button => {
+
+    button.addEventListener("click", NumberClick);
+    
+});
+
+
+
